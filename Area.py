898684a94ffd2c1e69ddc9ad21f72779a2b522d9
samruddd <87090108+samruddd@users.noreply.github.com>
2021-10-17 01:39:48 +0530

    Update Area.py

--- Area.py
+++ Area.py
@@ -3,3 +3,18 @@ from math import pi
 r = float(input ("Enter radius of circle : "))
 
 print ("Area of the circle is: " + str(pi * r**2))
+
+
+
+
+
+
+
+
+
+
+
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
